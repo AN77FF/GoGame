e431536b69492bb3983bbd606d172256a491d82c
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-13 18:53:36 +0400

    ViewModel.cs

--- Base/ViewModel.cs
+++ Base/ViewModel.cs
@@ -1,1 +1,11 @@
+public class ViewModel : INotifyPropertyChanged, IViewModel
+{
+    public event PropertyChangedEventHandler PropertyChanged;
 
+    public virtual void Initialize() { /* Initialization logic */ }
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
