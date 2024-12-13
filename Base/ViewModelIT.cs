180cbb44144b7b8c0508e4650dbaa8ef0a1dc06b
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-13 18:54:51 +0400

    ViewModelIT.cs

--- Base/ViewModelIT.cs
+++ Base/ViewModelIT.cs
@@ -1,1 +1,13 @@
-
+public class ViewModelT<T> : ViewModel
+{
+    private T _data;
+    public T Data
+    {
+        get => _data;
+        set
+        {
+            _data = value;
+            OnPropertyChanged(nameof(Data));
+        }
+    }
+}
