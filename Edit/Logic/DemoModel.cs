897694f4519599d029e6f7a6c10d901cc5b80b19
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-13 20:51:22 +0400

    DemoModel.cs

--- Edit/Logic/DemoModel.cs
+++ Edit/Logic/DemoModel.cs
@@ -3,4 +3,9 @@ public class DemoModel
     public string Name { get; set; }
     public int Age { get; set; }
 
+    public DemoModel(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
 }
