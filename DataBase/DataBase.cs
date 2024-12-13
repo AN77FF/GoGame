e69f2eaac515631481ed559e2d3b7db404ddcf9a
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-13 19:01:00 +0400

    DataBase.cs

---
+++ DataBase/DataBase.cs
@@ -0,0 +1,28 @@
+public class DataBase : IDataBase
+{
+    private GoBoard _board;
+
+    public DataBase(GoBoard board)
+    {
+        _board = board;
+    }
+
+    public void Save(string filePath)
+    {
+        // Сериализация состояния доски в файл
+        using (var writer = new StreamWriter(filePath))
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(GoBoard));
+            serializer.Serialize(writer, _board);
+        }
+    }
+
+    public void Load(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(GoBoard));
+            _board = (GoBoard)serializer.Deserialize(reader);
+        }
+    }
+}
