2a15317f15d027dffb405c78673b27e1aef0f711
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-14 12:04:08 +0400

    ViewModel.cs

--- Base/ViewModel.cs
+++ Base/ViewModel.cs
@@ -2,7 +2,37 @@ public class ViewModel : INotifyPropertyChanged, IViewModel
 {
     public event PropertyChangedEventHandler PropertyChanged;
 
-    public virtual void Initialize() { /* Initialization logic */ }
+    private List<CpuModels> _cpuList;
+    public List<CpuModels> CpuList
+    {
+        get => _cpuList;
+        set
+        {
+            if (_cpuList != value)
+            {
+                _cpuList = value;
+                OnPropertyChanged(nameof(CpuList));
+            }
+        }
+    }
+
+    public virtual void Initialize()
+    {
+        
+        LoadCpuList();
+    }
+
+    private void LoadCpuList()
+    {
+        
+        CpuList = new List<CpuModels>
+        {
+            new CpuModels { Name = "Intel Core i7", Cores = 8, Speed = 3.6 },
+            new CpuModels { Name = "AMD Ryzen 5", Cores = 6, Speed = 3.4 },
+            new CpuModels { Name = "Intel Core i5", Cores = 4, Speed = 3.0 },
+            new CpuModels { Name = "AMD Ryzen 7", Cores = 8, Speed = 3.5 }
+        };
+    }
 
     protected void OnPropertyChanged(string propertyName)
     {
