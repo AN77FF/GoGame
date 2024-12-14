e6a35f57e649e9264734a2e3cc4869694f06b5cb
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-14 11:46:03 +0400

    DemoModel.cs

--- Edit/ShowCpuList/Logic/DemoModel.cs
+++ Edit/ShowCpuList/Logic/DemoModel.cs
@@ -1,1 +1,11 @@
+public class CpuModels
+{
+    public string Name { get; set; }
+    public int Cores { get; set; }
+    public double Speed { get; set; }
 
+    public override string ToString()
+    {
+        return $"{Name} - {Cores} cores, {Speed} GHz";
+    }
+}
