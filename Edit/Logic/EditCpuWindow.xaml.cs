4ef7a052c592fe27a9d099d444c0314254e33d32
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-14 11:41:55 +0400

    EditCpuWindow.xaml.cs

--- Edit/Logic/EditCpuWindow.xaml.cs
+++ Edit/Logic/EditCpuWindow.xaml.cs
@@ -1,1 +1,10 @@
+using System.Windows;
 
+public partial class EditCpuWindow : Window
+{
+    public EditCpuWindow(CpuModels model)
+    {
+        InitializeComponent();
+        DataContext = new EditCpuViewModel(model);
+    }
+}
