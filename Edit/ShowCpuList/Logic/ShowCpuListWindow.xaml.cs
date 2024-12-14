9b146f16e42d50ec4c141857df75f67d47b17501
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-14 11:44:28 +0400

    ShowCpuListWindow.xaml.cs

--- Edit/ShowCpuList/Logic/ShowCpuListWindow.xaml.cs
+++ Edit/ShowCpuList/Logic/ShowCpuListWindow.xaml.cs
@@ -1,1 +1,24 @@
+using System.Collections.Generic;
+using System.Windows;
 
+public partial class ShowCpuListWindow : Window
+{
+    public List<CpuModels> CpuList { get; set; }
+
+    public ShowCpuListWindow()
+    {
+        InitializeComponent();
+        LoadCpuList();
+        DataContext = this;
+    }
+
+    private void LoadCpuList()
+    {
+        
+        CpuList = new List<CpuModels>
+        {
+            new CpuModels { Name = "Intel Core i7", Cores = 8, Speed = 3.6 },
+            new CpuModels { Name = "AMD Ryzen 5", Cores = 6, Speed = 3.4 }
+        };
+    }
+}
