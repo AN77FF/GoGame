3c68efff111db0e003dcc6a96d89780749b01b07
AN77FF <151375998+AN77FF@users.noreply.github.com>
2024-12-18 16:26:43 +0400

    Locator.cs

--- Locator.cs
+++ Locator.cs
@@ -1,8 +1,6 @@
 public static class Locator
 {
     public static IViewService ViewService { get; } = new ViewService();
-    public static IEditWindowProvider EditWindowProvider { get; } = new EditCpuWindowProvider();
-    public static IShowCpuListWindowProvider ShowCpuListWindowProvider { get; } = new ShowCpuListWindowProvider();
     public static IDataBase DataBase { get; } = new DataBase();
     public static IViewModel ViewModel { get; } = new ViewModel();
     public static IDispatcherHelper DispatcherHelper { get; } = new DispatcherHelper();
